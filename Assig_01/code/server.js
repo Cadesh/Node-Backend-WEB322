8bd0732774553f9c304ba2e35bc052939da98601
Andre <andrewvrosa@gmail.com>
2018-07-14 20:57:27 -0400

    learning Heroku

---
+++ Assig_01/code/server.js
@@ -0,0 +1,13 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+
+// setup a 'route' to listen on the default url path
+app.get("/", (req, res) => {
+    res.send("Teste de Aplicativo - Map-Test - update");
+});
+
+
+// setup http server to listen on HTTP_PORT
+app.listen(HTTP_PORT);
+
